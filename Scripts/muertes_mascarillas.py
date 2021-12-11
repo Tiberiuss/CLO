cdc8f6eafd0f61c73e65555e208d7d9861c08b23
inirolga <87582673+inirolga@users.noreply.github.com>
2021-12-11 16:57:43 +0100

    Add files via upload

--- Scripts/muertes_mascarillas.py
+++ Scripts/muertes_mascarillas.py
@@ -24,14 +24,14 @@ spark.sparkContext.addFile(urlfile3)
 spark.sparkContext.addFile(urlfile4)
 
 df_spark_governmentResponse = spark.read.option('header','true').option('inferSchema','true').csv("file://"+SparkFiles.get("oxford-government-response.csv"))\
-    .filter(col("location_key").isin(paisesLista)).filter(col("stringency_index") != 0)
+    .filter(col("location_key").isin(paisesLista)).filter(col("facial_coverings") != 0)
 
 for x in paisesLista:
     df_spark_governmentResponse_pais = df_spark_governmentResponse.filter(col("location_key") == x)
     df_spark_epidemiology = spark.read.option('header','true').option('inferSchema','true').csv("file://"+SparkFiles.get("epidemiology.csv")).filter(col("location_key") == x)
+
     df_spark_demographics = spark.read.option('header','true').option('inferSchema','true').csv("file://"+SparkFiles.get("demographics.csv")).filter(col("location_key") == x)
-    df_spark_hospitalizations = spark.read.option('header','true').option('inferSchema','true').csv("file://"+SparkFiles.get("hospitalizations.csv")).filter(col("location_key") == x)
-    
+
     df_spark_hospitalizations = spark.read.option('header','true').option('inferSchema','true').csv("file://"+SparkFiles.get("hospitalizations.csv")).filter(col("location_key") == x)
     poblacion = df_spark_demographics.first()["population"]
 
@@ -39,11 +39,11 @@ for x in paisesLista:
     dfJOIN = dfJOIN.join(df_spark_hospitalizations, 'date').orderBy(col("date").asc())
 
     proporcion = 1000000 / poblacion
-    
     dfJOIN = dfJOIN.withColumn("new_deceased", col("new_deceased") * proporcion).withColumnRenamed("new_deceased", "new_deceased_1000000")
+    dfJOIN = dfJOIN.withColumn("facial_coverings", col("facial_coverings") * 25).withColumnRenamed("facial_coverings", "facial_coverings_percentage")
     dfJOIN = dfJOIN.withColumn("new_hospitalized_patients", col("new_hospitalized_patients") * proporcion).withColumnRenamed("new_hospitalized_patients", "new_hospitalized_patients_100000")
 
     dfJOIN.show()
     df_panda = dfJOIN.toPandas()
-    df_panda.plot(x ='date', y=["stringency_index", "new_deceased_1000000", "new_hospitalized_patients_100000"], kind = 'line')
-    plt.savefig("graficos/muertes_restricciones" + x + ".jpeg")
+    df_panda.plot(x ='date', y=["facial_coverings_percentage", "new_deceased_1000000", "new_hospitalized_patients_100000"], kind = 'line')
+    plt.savefig("graficos/muertes_mascarillas" + x + ".jpeg")
